dcb60ad1126c80cd170f8c7dfe093ce0760dfab9
nikolay <nikolaygalchev@gmail.com>
2019-01-24 21:45:13 +0200

    Adding new package tickets with class Ticket.java

---
+++ src/tickets/Ticket.java
@@ -0,0 +1,142 @@
+package tickets;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class Ticket {
+	
+	
+	private static final int INVALID_TICKET_PRICE = 5;
+	private static final int STUDENT_TICKET_PRICE = 8;
+	private static final int CHILD_TICKET_PRICE = 6;
+	private static final int STANDART_TICKET_PRICE = 10;
+
+
+//	private Cinema cinema;
+//	private Movie movie;
+	private int price;
+	private Seat seat;
+	private String type;
+	private static int id = 0;
+	private int serialNumber;
+	
+	private static final Set<String> ticketTypes = new HashSet<>(Arrays.asList(
+			new String("STANDART TICKET"),
+			new String("CHILD TICKET"),
+			new String("INVALID TICKET"),
+			new String("STUDENT TICKET")
+	));
+	
+	private Ticket(String seat, int price /*, Cinema cinema, Movie movie */) {
+		this.seat = new Seat(seat);
+		setPrice(price);
+		this.serialNumber = ++Ticket.id; // unique serial number
+		//TO DO this. cinema = cinema, this movie = movie
+	}
+	
+	
+	private class Seat{
+		
+		private static final char MAX_ROW = 'Z';
+		private static final char MIN_ROW = 'A';
+		
+		private static final char MIN_NUMBER_PLACE = '0';
+		private static final int MAX_NUMBER_PLACE = 15;
+		
+		
+		private String row;
+		private int place;
+		private boolean isFree;
+		
+		public Seat(String stringSeat) {
+			stringSeat = stringSeat.toUpperCase();
+			if(isValidRow(stringSeat)) {
+				this.row = stringSeat.substring(0, 1);
+			} else {
+				System.out.println("That is invalid row");
+			}
+			
+			if(isValidPlace(stringSeat)) {
+				this.place = Integer.parseInt(stringSeat.substring(1));
+			} else {
+				System.out.println("That is invalid place");
+			}
+		}
+
+		private boolean isValidPlace(String stringSeat) {
+			return Integer.parseInt(stringSeat.substring(1, stringSeat.length())) <= MIN_NUMBER_PLACE 
+					&& Integer.parseInt(stringSeat.substring(1, stringSeat.length())) >= 0;
+		}
+
+		private boolean isValidRow(String stringSeat) {
+			return (stringSeat.charAt(0) >= MIN_ROW && stringSeat.charAt(0) <= MAX_ROW)
+					|| (stringSeat.charAt(0) >= 'a' && stringSeat.charAt(0) <= 'z');
+		}
+		
+		public String getSeatValue() {
+			if(this.row != null) {
+				return (this.row + " - " + ("" + this.place));
+			} else {
+				return "Sorry the ticket is null";
+			}
+		}
+	}
+		
+	
+	public void setPrice(int price) {
+		if(price > 0) {
+			this.price = price;
+		} else {
+			System.out.println("Ticket price must be a positive value");
+		}
+	}
+	
+
+	
+
+	public static Ticket getInstance(String ticketType, String seat) {
+		String newTicketType = ticketType.toUpperCase();
+		
+		if(Ticket.ticketTypes.contains(newTicketType)) {
+			String ticket = ticketType;
+			
+			if(ticket.equalsIgnoreCase("Standart Ticket")) {
+				return new Ticket(seat, STANDART_TICKET_PRICE);
+			}
+			
+			if(ticket.equalsIgnoreCase("Child Ticket")) {
+				return new Ticket(seat, CHILD_TICKET_PRICE);
+			}
+			
+			if(ticket.equalsIgnoreCase("Student Ticket")) {
+				return new Ticket(seat, STUDENT_TICKET_PRICE);
+			}
+			
+			if(ticket.equalsIgnoreCase("Invalid Ticket")) {
+				return new Ticket(seat, INVALID_TICKET_PRICE);
+			}
+		} 
+		
+		System.out.println("We do not have that ticket type, Sorry");
+		//TO DO throw exception (spored men) i tva return null nqma da go ima
+		return null;
+		
+	}
+
+	public Seat getSeat() {
+		return seat;
+	}
+	
+
+
+
+
+
+
+
+
+		
+	
+}
