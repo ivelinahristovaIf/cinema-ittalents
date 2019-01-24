9fba68b7105ada301c5449e23332b9e644fff74b
Ivelina Hristova <ivelinahristova5@gmail.com>
2019-01-24 23:52:23 +0200

    Adding menu to DemoCinema.java

--- src/cinema/DemoCinema.java
+++ src/cinema/DemoCinema.java
@@ -1,5 +1,8 @@
 package cinema;
 
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
 import tickets.NotValidTicketTypeException;
 import tickets.Ticket;
@@ -8,8 +11,63 @@ import users.Admin;
 import users.Consumer;
 
 public class DemoCinema {
+	public static Admin admin = DemoCinema.createAdmin("admin", "admin");
+	// TODO how to make consumers non static
+	private static Set<Consumer> consumers;
+
 	public static void menu() {
-		System.out.println("Изберете");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Изберете опция от менюто: ");
+		System.out.println("1 -> Вход за администратор...");
+		System.out.println("2 -> Вход за потребител...");
+		System.out.println("3 -> Регистриране на нов потребител...");
+		int option = sc.nextInt();
+		if (option == 1) {
+			System.out.println("Администратор");
+			System.out.println("Въведете потребителско име: ");
+			String username = sc.next();
+			System.out.println("Въведете парола: ");
+			String password = sc.next();
+			if (isValidUsernameAndPassword(username, password)) {
+				admin.showMenu();
+			}
+		}
+		if (option == 2) {
+			System.out.println("Потребител");
+			System.out.println("Въведете email : ");
+			String username = sc.next();
+			System.out.println("Въведете парола: ");
+			String password = sc.next();
+		}
+		if (option == 3) {
+			System.out.println("Регистрирайте се като потребител...");
+			Consumer consumer = new Consumer(1);
+			// TODO add consumer to Cinema
+			System.out.println(consumer + " е регистриран");
+			consumers.add(consumer);
+		}
+
+	}
+
+	private static boolean isValidUsernameAndPassword(String username, String password) {
+		if (username != null) {
+			if (password != null) {
+				if (username.equals(DemoCinema.admin.getUsername()) && password.equals(admin.getPassword())) {
+					System.out.println("Вписахте се успешно като администратор!");
+					return true;
+				}
+			}
+			System.out.println("Null admin password");
+			return false;
+		}
+		System.err.println("Null admin username");
+		return false;
+	}
+
+	public static Admin createAdmin(String username, String password) {
+		// TODO Singleton
+		// TODO is correct username and password
+		return new Admin(username, password);
 	}
 
 	public static void main(String[] args) {
@@ -20,13 +78,10 @@ public class DemoCinema {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+//		Admin admin = new Admin("admin", "admin");
+		consumers = new HashSet<Consumer>();
+		menu();
 
-	
-	
-	
-	
-	
-	}	
-	
+	}
+
 }
--- src/users/Admin.java
+++ src/users/Admin.java
@@ -7,6 +7,20 @@ public class Admin {
 	public Admin(String username, String password) {
 		this.username = username;
 		this.password = password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void showMenu() {
+		System.out.println("Изберете опция от администраторското меню:");
+		//TODO add movie, change program etc
+		
 	};
 	
 
