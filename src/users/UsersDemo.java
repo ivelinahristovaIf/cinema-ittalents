6fbf7aa73a09ecfb0db3263cf3e7cc8dc645207b
nikolay <nikolaygalchev@gmail.com>
2019-01-25 16:52:27 +0200

    adding method buyTicket in class Consumer

--- src/users/UsersDemo.java
+++ src/users/UsersDemo.java
@@ -1,15 +1,24 @@
 package users;
 
+import cinema.Cinema;
+import tickets.NotValidTicketTypeException;
+import tickets.Ticket;
+
 public class UsersDemo {
 
 	public static void main(String[] args) {
 		Consumer ivan = new Consumer(1);
-		ivan.setUpMyProfile();
+//		ivan.setUpMyProfile();
 		System.out.println(ivan);
 
 		
 //		ivan.changePassword();
 //		ivan.setInterests();
+		
+		
+		
+
+		
 	}
 
 }
